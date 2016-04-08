dca5ce351efb9f8871b41db7d04113256e9798b8
Andrei Gherzan <andrei@resin.io>
2016-04-08 05:53:42 +0200

    Add support for display port
    Signed-off-by: Andrei Gherzan <andrei@resin.io>

--- bin/i3display.sh
+++ bin/i3display.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 if [ "$1" = "dual" ]; then
-  xrandr --output HDMI2 --auto --right-of eDP1 --output DP1 --auto
-fi
-if [ "$1" = "hdmi" ]; then
-  xrandr --output HDMI2 --auto --output eDP1 --off
+  xrandr --output DP1 --auto --panning 2880x1800+2560+0 --scale 1.5x1.5 --right-of eDP1 --output eDP1 --auto
 fi
 if [ "$1" = "laptop" ]; then
-  xrandr --output HDMI2 --off --output eDP1 --auto
+  xrandr --output eDP1 --auto --output HDMI2 --off --output DP1 --off
 fi
-
+if [ "$1" = "extern" ]; then
+  xrandr --output DP1 --auto --panning 2880x1800+0+0 --scale 1.5x1.5 --output HDMI2 --auto --output eDP1 --off
+fi
+if [ "$1" = "hdmi" ]; then
+  xrandr  --output eDP1 --off --output DP1 --off --output HDMI2 --mode 1920x1080i
+fi
 ~/.fehbg
