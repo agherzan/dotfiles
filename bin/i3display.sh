629d6944aecfdcd9afa168c027d15dc57414936f
Andrei Gherzan <andrei@resin.io>
2016-08-25 12:12:08 +0100

    i3: Add docking configuration
    Signed-off-by: Andrei Gherzan <andrei@resin.io>

--- bin/i3display.sh
+++ bin/i3display.sh
@@ -3,6 +3,12 @@
 if [ "$1" = "dual" ]; then
   xrandr --output DP1 --auto --panning 2880x1800+2560+0 --scale 1.5x1.5 --right-of eDP1 --output eDP1 --auto
 fi
+if [ "$1" = "docking-dual" ]; then
+  xrandr --output DP2 --auto --panning 2880x1800+2560+0 --scale 1.5x1.5 --right-of eDP1 --output eDP1 --auto
+fi
+if [ "$1" = "docking-only" ]; then
+  xrandr --output DP2 --auto --panning 2880x1800+0+0 --scale 1.5x1.5 --output HDMI2 --auto --output eDP1 --off
+fi
 if [ "$1" = "laptop" ]; then
   xrandr --output eDP1 --auto --output HDMI2 --off --output DP1 --off
 fi
